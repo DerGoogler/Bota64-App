5d6e478da15bd757fbeeff1e07735def8435663e
Der_Googler <jimmyboehm@dergoogler.com>
2022-07-11 12:20:26 +0200

    hmm

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,8 +1,9 @@
 import { merge } from "webpack-merge";
-import { defConfig, config } from "./webpack.config";
+import { defConfig, config, defPlugins } from "./webpack.config";
 
 export default merge(config, {
   mode: "development",
   ...defConfig,
   devtool: "source-map",
+  plugins: defPlugins,
 });
